8f73a17dae3fd8fb38a96afc664c2615f42a3f00
zachbeeler <zach@scalesec.com>
2022-05-24 16:29:13 -0500

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "<zach+testaft@scalesec.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "<zach@scalesec.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
