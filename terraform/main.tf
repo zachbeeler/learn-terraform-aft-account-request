1ec7796804620bb9d94648e51004e5edb146d280
zachbeeler <zach@scalesec.com>
2022-05-25 10:38:42 -0500

    Trying for the fourth time, no carrot

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<zach+thirdtestaft@scalesec.com"
-    AccountName               = "sandbox-af-3"
+    AccountEmail              = "zach+fourthtestaft@scalesec.com"
+    AccountName               = "sandbox-aft-4"
     ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "<zach@scalesec.com"
+    SSOUserEmail              = "zach@scalesec.com"
     SSOUserFirstName          = "Zach"
     SSOUserLastName           = "AFT"
   }
